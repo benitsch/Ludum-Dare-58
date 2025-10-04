e7882a95e3f5ac4b7730102d17d9e30f4b905c4a
nitber <bernhardnitsch@gmx.at>
2025-10-04 14:10:46 +0200

    add leaf animation when butterfly wings falling down

--- Scenes/butterfly_wing.gd
+++ Scenes/butterfly_wing.gd
@@ -1,5 +1,17 @@
 extends Sprite2D
 
+var falling = false
+var speed = 150.0
+var sway_speed = 2.0
+var sway_amplitude = 80.0
+var time = 0.0
+
+func _process(delta):
+	if falling:
+		time += delta
+		position.y += speed * delta
+		position.x += sin(time * sway_speed) * sway_amplitude * delta
+		rotation = sin(time * sway_speed) * 0.2  # leichtes Drehen
+
 func fallDown() -> void:
-	var tween = get_tree().create_tween()
-	tween.tween_property($".", "position:y", $".".position.y + 300, 1.0)
+	falling = true
