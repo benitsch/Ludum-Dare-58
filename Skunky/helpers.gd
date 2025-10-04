4670e50715de6952f6ba3c3ef04ac65aab881120
Skunky <43096109+Skunkynator@users.noreply.github.com>
2025-10-04 17:26:17 +0200

    update helper function

--- Skunky/helpers.gd
+++ Skunky/helpers.gd
@@ -7,5 +7,5 @@ class_name SkunkyHelper
 ## b is the target value[br]
 ## delta is current delta time[br]
 ## half_life is the amount of seconds until the value goes halfway to the target
-static func lerp_smooth(a: float, b: float, delta: float, half_life: float):
-	return b + (a-b) * pow(2, -delta / half_life);
+static func lerp_smooth(a: float, b: float, delta: float, half_life: float) -> float:
+	return lerp(a,b, 1 - pow(2, -delta / half_life));
