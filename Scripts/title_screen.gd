c1b93e0c1d333c2cc246398f1a1f728bb332f41d
nitber <bernhardnitsch@gmx.at>
2025-10-04 13:21:50 +0200

    Basic title screen

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,6 +1,20 @@
 extends Control
 
+var userPressedStartButton = false
+var currentShakes = 0
+var maxShakesAllowed = 4
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level1.tscn")
+	$Camera2D/AnimationPlayer.play("ZoomOut")
+	userPressedStartButton = true
+	#get_tree().change_scene_to_file("res://Scenes/level1.tscn")
 	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if userPressedStartButton && event is InputEventKey:
+		if event.keycode == KEY_A || event.keycode == KEY_D:
+			currentShakes += 1
+			$MainButterfly/ShakerComponent2D.play_shake()
+			$ButterflyBoard/ShakerComponent2D.play_shake()
+			if currentShakes == 1:
+				$ButterflyWing.fallDown()
