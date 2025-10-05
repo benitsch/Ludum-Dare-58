22c5567e529d6f1a7321dfd215141b7e7c592a85
Skunky <43096109+Skunkynator@users.noreply.github.com>
2025-10-05 11:07:16 +0200

    update death area to be directiona

--- Skunky/death_area.gd
+++ Skunky/death_area.gd
@@ -1,10 +1,15 @@
 extends Area2D
 
 
+@export var kill_directional: bool = false
+
+
 func _init() -> void:
 	connect("body_entered", body_entered)
 
 
 func body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.kill();
+		var player := body as Player
+		if not kill_directional or global_transform.y.dot(player.velocity) > 0:
+			player.kill();
