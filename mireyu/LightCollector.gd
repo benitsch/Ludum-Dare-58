4757f0d76010c155ab7b2a91cf66c5220c884afb
mireyu <mireyu@anx.io>
2025-10-06 00:20:38 +0200

    Add sprite, modify collision, add states for light collector

--- mireyu/LightCollector.gd
+++ mireyu/LightCollector.gd
@@ -2,11 +2,18 @@ extends AnimationTrigger
 
 class_name LightCollector
 
+@onready var sprite: Sprite2D = $Sprite2D
+var inactiveState: Texture2D = preload("res://Solar01.png")
+var activeState: Texture2D = preload("res://Solar02.png")
+
+
 func _init() -> void:
 	onlyTriggerOnce = true
 
 func onBeamHit() -> void:
 	animateTarget()
+	sprite.set_texture(activeState)
 
 func onBeamLeave() -> void:
 	animateTarget(true)
+	sprite.set_texture(inactiveState)
