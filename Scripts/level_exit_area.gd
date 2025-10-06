c356017243b925c144768c4d1d70ad3d324a9ff3
mireyu <mireyu@anx.io>
2025-10-07 00:45:14 +0200

    Fix circular reference error by simply not setting the PackedScene and directly changing to the EndScreen

--- Scripts/level_exit_area.gd
+++ Scripts/level_exit_area.gd
@@ -8,12 +8,14 @@ extends Area2D
 func _init() -> void:
 	connect("body_entered", body_entered)
 
-
 func body_entered(body: Node2D) -> void:
 	if not body is Player: return
-	if not next_scene: return
 	var player := body as Player
 	animator.play("fade_in")
 	player.end_level()
 	await animator.animation_finished
-	get_tree().change_scene_to_packed(next_scene)
+	
+	if not next_scene: 
+		get_tree().change_scene_to_file("res://Scenes/EndScreen.tscn")
+	else:
+		get_tree().change_scene_to_packed(next_scene)
